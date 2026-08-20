8c843e8f57a2dc9f40f964b2fe7c9bddb9a75b52
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2026-08-20 21:39:42 +0300

    Implemented the View to display rentals by the filters and the Rental template

--- YourRoom/urls.py
+++ YourRoom/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from your_room.views import index, rental
+from your_room.views import index, rental, rental_detail
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path("rentals/", rental, name = "rentals")
+    path("rentals/", rental, name = "rentals"),
+    path("rentals/<int:pk>/", rental_detail, name="rental-detail")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- your_room/views.py
+++ your_room/views.py
@@ -1,8 +1,38 @@
 
-from django.shortcuts import render
+from your_room.models import Rental, Hostel, Airbnb
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return render(request, "your_room/index.html")
 
 def rental(request):
-    return render(request, "your_room/rentals.html")
+    units = Rental.objects.prefetch_related("images")
+
+    location = request.GET.get("location")
+    room_type = request.GET.get("room_type")
+    self_contained = request.GET.get("self_contained")
+
+    if location:
+        units = units.filter(location__icontains=location)
+    if room_type:
+        units = units.filter(room_type=room_type)
+    if self_contained in ("true", "false"):
+        units = units.filter(self_contained=(self_contained == "true"))
+
+    context = {
+        "units": units,
+        # feeds the location dropdown with whatever locations actually exist
+        "locations": Rental.objects.values_list("location", flat=True).distinct(),
+    }
+    return render(request, "your_room/rentals.html", context)
+
+UNIT_MODELS = { 'rental' : Rental, 
+               'hostel' : Hostel, 
+               'airbnb' : Airbnb}
+
+# Unit_type is a parameter added to the link sending the request
+# If the link has a unit type equal to the model in the dictionary, detail will only display that list
+def unit_detail(request,unit_type, pk): 
+    model = UNIT_MODELS.get(unit_type)
+    unit = get_object_or_404(model.objects.prefetch_related("images"), pk=pk)
+    return render(request, "your_room/detail.html", {"unit": unit})
