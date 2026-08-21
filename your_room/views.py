ecfef89fcd2171b59db9311a940053f6c849f8a3
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2026-08-21 16:01:09 +0300

    Added a dropdown as the search bar

--- your_room/views.py
+++ your_room/views.py
@@ -2,8 +2,19 @@
 from your_room.models import Rental, Hostel, Airbnb
 from django.shortcuts import render, get_object_or_404
 
+UNIT_MODELS = { 'rental' : Rental, 
+               'hostel' : Hostel, 
+               'airbnb' : Airbnb}
+
+
 def index(request):
-    return render(request, "your_room/index.html")
+    location = Rental.objects.values_list("location", flat=True).distinct()
+    location = Airbnb.objects.values_list("location", flat=True).distinct()
+    location = Hostel.objects.values_list("university", flat=True).distinct()
+
+    context = { "locations" : location }
+
+    return render(request, "your_room/index.html", context)
 
 def rental(request):
     units = Rental.objects.prefetch_related("images")
@@ -26,9 +37,6 @@ def rental(request):
     }
     return render(request, "your_room/rentals.html", context)
 
-UNIT_MODELS = { 'rental' : Rental, 
-               'hostel' : Hostel, 
-               'airbnb' : Airbnb}
 
 # Unit_type is a parameter added to the link sending the request
 # If the link has a unit type equal to the model in the dictionary, detail will only display that list
@@ -60,3 +68,6 @@ def hostel(request):
 
 def airbnb(request):
     return render(request, "your_room/airbnb.html")
+
+def search(request):
+    return render(request, "your_room/search.html")
